a1ed364e1e7bebc0493edeaa8947c5a1dae43bb9
gabeaster <gab.easter@live.com>
2020-04-24 11:29:57 -0400

    installed dependencies, built axiosWithAuth in src/utils, built Login form withRouter in components

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,75 @@
-import React from "react";
+import React, { useState, render } from "react";
+import { withRouter } from "react-router-dom";
+import axios from "axios";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const LoginForm = (props) => {
+  const [name, setName] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setName({ ...name, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const login = (e) => {
+    e.preventDefault();
+    setName({ ...name, isLoading: true });
+    axios
+      .post("http://localhost:5000/api/login", name)
+      .then((res) => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+      })
+      .catch((err) => console.log(err.response));
+    props.history.push("/protect");
+  };
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <div className="login-card">
+      <form
+        className="login-modal"
+        onSubmit={(e) => handleSubmit(e)}
+        onKeyDown={props.closeLoginHandler2}
+        tabIndex="0"
+      >
+        <h1 className="title">Title</h1>
+        <p clasName="login-inputs">
+          <label>
+            Username:
+            <input
+              className="input-forms"
+              type="text"
+              name="username"
+              onChange={changeHandler}
+              value={name.username}
+            />
+          </label>
+        </p>
+        <p className="login-inputs">
+          <label>
+            Password:{" "}
+            <input
+              className="input-forms"
+              type="password"
+              onChange={changeHandler}
+              value={name.password}
+            />
+          </label>
+        </p>
+        <button type="submit" className="login-button" onClick={login}>
+          Login
+        </button>
+      </form>
+    </div>
   );
 };
 
-export default Login;
+export default withRouter(LoginForm);
